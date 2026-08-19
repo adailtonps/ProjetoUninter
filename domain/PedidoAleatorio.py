a45889a618ed424e63ad0b28b2543fa648a689f9
adailton@ps.com <adailtonpereirasilveira9@gmail.com>
2026-08-19 14:23:17 -0300

    mudando as listas de ingredientes de pedido_aleatorio

--- domain/PedidoAleatorio.py
+++ domain/PedidoAleatorio.py
@@ -1,1 +1,35 @@
 
+comidas = [
+    "hamburguer",
+    "pizza",
+    "sopa"
+]
+
+ingredientes_hamburguer = [
+    "carne",
+    "tomate",
+    "queijo",
+    "alface",
+    "ketchup",
+    "mostarda"
+]
+
+ingredientes_pizza = [
+    "molho_tomate",
+    "queijo",
+    "alface",
+    "cebola",
+    "salame",
+    "cogumelo",
+    "ketchup",
+    "mostarda"
+]
+
+ingredientes_sopa = [
+    "batata",
+    "cenoura",
+    "beterraba",
+    "abobora",
+    "carne_frango",
+    "cebola"
+]
